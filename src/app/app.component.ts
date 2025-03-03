f1457dc91f65095e148feead0bbb0d276c86c5ba
DanieZapata <zapataparidaniela@gmail.com>
2025-03-03 00:26:34 -0400

    add: card component redirection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent,],
+  imports: [SidebarComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
