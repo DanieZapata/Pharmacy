903e6991bf9b229ee002c061a2450fd9e08768f8
DanieZapata <zapataparidaniela@gmail.com>
2025-03-03 23:00:17 -0400

    update: view table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { CardComponent } from './card/card.component';
+import { TableComponent } from './table/table.component';
+//import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, CardComponent],
+  imports: [SidebarComponent, TableComponent/*CardComponent*/],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
