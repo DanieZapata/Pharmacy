c0f2a4d5d6b60d4370d55ab004df73191ebec825
DanieZapata <zapataparidaniela@gmail.com>
2025-03-04 21:04:13 -0400

    add: search function

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule,CommonModule,MatIconModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  searchTerm: string = '';
 
+  @Output() searchChange = new EventEmitter<string>();
+
+  onSearch() {
+    this.searchChange.emit(this.searchTerm);
+  }
+
+  clearSearch() {
+    this.searchTerm = '';
+    this.onSearch();
+  }
 }
