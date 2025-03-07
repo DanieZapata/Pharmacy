219a1ef419ee206f4b090af80cea0014b0f84fae
DanieZapata <zapataparidaniela@gmail.com>
2025-03-06 22:11:24 -0400

    add: redirect from sidebar to add product

--- src/app/pages/add-products/add-products.component.ts
+++ src/app/pages/add-products/add-products.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableComponent } from "../../components/table/table.component";
 
 @Component({
   selector: 'app-add-products',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TableComponent],
   templateUrl: './add-products.component.html',
   styleUrl: './add-products.component.css'
 })
