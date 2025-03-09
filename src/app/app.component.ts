e1daa279cea8c1999fad88c6b784fe1a58f2bfc0
DanieZapata <zapataparidaniela@gmail.com>
2025-03-08 23:14:07 -0400

    add: show modal button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common'; 
+import { ProductRegistrationComponent } from './components/product-registration/product-registration.component';
 //import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, RouterModule, CommonModule /*CardComponent*/],
+  imports: [SidebarComponent, RouterModule, CommonModule, ProductRegistrationComponent /*CardComponent*/],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
