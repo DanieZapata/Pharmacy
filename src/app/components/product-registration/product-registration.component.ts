43be6bf434d3ddf9b43afc4aa54e772c314433eb
DanieZapata <zapataparidaniela@gmail.com>
2025-03-08 23:09:39 -0400

    add: product registration form

--- src/app/components/product-registration/product-registration.component.ts
+++ src/app/components/product-registration/product-registration.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-registration',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, FormsModule],
   templateUrl: './product-registration.component.html',
   styleUrl: './product-registration.component.css'
 })
 export class ProductRegistrationComponent {
+  isModalOpen = false;
+
+  product = {
+    name: '',
+    lote: '',
+    cantidad: 0,
+    fechaVencimiento: '',
+    precio: 0,
+  };
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 
 }
