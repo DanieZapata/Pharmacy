d4c995d90e5c7139a0131d369a49cd2319aad33c
DanieZapata <zapataparidaniela@gmail.com>
2025-03-08 22:23:22 -0400

    add:registration button component

---
+++ src/app/components/product-registration/product-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-registration',
+  imports: [],
+  templateUrl: './product-registration.component.html',
+  styleUrl: './product-registration.component.css'
+})
+export class ProductRegistrationComponent {
+
+}
