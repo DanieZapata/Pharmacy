dea385669e2cf83b3a6220b1db072c15bf1c2c5e
DanieZapata <zapataparidaniela@gmail.com>
2025-03-17 21:29:14 -0400

    add:laboratory route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { AddProductsComponent } from './pages/add-products/add-products.component';
 import { TableComponent } from './components/table/table.component';
 import { ProductRegistrationComponent } from './components/product-registration/product-registration.component';
-import { CardComponent } from './components/card/card.component';
+import { LaboratoryComponent } from './pages/laboratory/laboratory.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, 
   { path: 'home', component: TableComponent },
   { path: 'add-products', component: ProductRegistrationComponent },
   { path: 'products', component: TableComponent },
-  { path: 'laboratories', component: CardComponent },
+  { path: 'laboratories', component: LaboratoryComponent },
 ];
