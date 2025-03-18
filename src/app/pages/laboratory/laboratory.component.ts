a806fac97421cb7934ae5a6c035ba952a27da944
DanieZapata <zapataparidaniela@gmail.com>
2025-03-17 21:39:43 -0400

    add:laboratories registration modal

--- src/app/pages/laboratory/laboratory.component.ts
+++ src/app/pages/laboratory/laboratory.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-laboratory',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './laboratory.component.html',
   styleUrl: './laboratory.component.css'
 })
 export class LaboratoryComponent {
+  isModalOpen = false;
+
+  laboratory = {
+    name: '',
+    phone: '',
+  };
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  submitForm() {
+    console.log('Laboratorio registrado:', this.laboratory);
+    this.closeModal();
+  }
 
 }
