10ed542c9b3c734b7e971e3431ce0111721cf3dd
DanieZapata <zapataparidaniela@gmail.com>
2025-03-23 22:09:24 -0400

    update:button menu list

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,11 +1,21 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Alterna la visibilidad del menú
+  }
+
+  action(action: string) {
+    console.log(`Acción seleccionada: ${action}`);
+    this.isMenuOpen = false; // Cierra el menú después de hacer una selección
+  }
 }
