caba84bb4df42053ce48e1568fd0f25faeaec77c
DanieZapata <zapataparidaniela@gmail.com>
2025-04-06 22:40:46 -0400

    add:service to obtain laboratories in the combobox

--- src/app/services/laboratory.serve.ts
+++ src/app/services/laboratory.serve.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Laboratory {
+  laboratoryId: number;
+  laboratoryName: string;
+}
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class LaboratoryService {
-  private laboratories = [
-    { id: 1, name: 'Laboratorio Central' },
-    { id: 2, name: 'BioTech Solutions' },
-  ];
+  private apiUrl = 'http://localhost:8081/laboratories';
 
-  getLaboratories() {
-    return this.laboratories;
+  constructor(private http: HttpClient) {}
+
+  getLaboratories(): Observable<Laboratory[]> {
+    return this.http.get<Laboratory[]>(this.apiUrl);
   }
 }
