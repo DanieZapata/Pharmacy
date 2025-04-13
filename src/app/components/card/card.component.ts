6da5cd55513ce671d1a496b661a7c9fc36a4320c
DanieZapata <zapataparidaniela@gmail.com>
2025-04-13 00:02:19 -0400

    add:issuance of laboratory data

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { HttpClient } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { Laboratory } from '../../services/laboratory.service';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent implements OnInit {
+  @Output() editarLaboratorio = new EventEmitter<Laboratory>(); 
+
   laboratorios: any[] = [];
 
   constructor(private http: HttpClient) {}
@@ -30,5 +35,9 @@ export class CardComponent implements OnInit {
         console.error('Error al obtener los laboratorios:', error);
       }
     });
+  }
+
+  editar(lab: Laboratory) {
+    this.editarLaboratorio.emit(lab);
   }
 }
